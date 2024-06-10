4c9df049d37a42c95b4b2665b2fdc68c92471380
Hemant Medhsia <142498656+MyCoddingJourney@users.noreply.github.com>
2024-06-10 18:13:29 +0530

    Final Commit of the project

--- constants/index.ts
+++ constants/index.ts
@@ -62,7 +62,7 @@ export const FOOTER_LINKS = [
   },
   {
     title: 'Our Community',
-    links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
+    links: ['Indian Climbing House', 'Mountains Guids', 'Top Supporter'],
   },
 ];
 
@@ -70,7 +70,7 @@ export const FOOTER_CONTACT_INFO = {
   title: 'Contact Us',
   links: [
     { label: 'Admin Officer', value: '123-456-7890' },
-    { label: 'Email Officer', value: 'hilink@akinthil.com' },
+    { label: 'Email Officer', value: 'hemantmedhsia@gmail.com' },
   ],
 };
 
